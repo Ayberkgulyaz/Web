c65a31a39cdb75c11423cbc14812d795946519e0
Your Name <ayberk_gulyaz@outlook.com>
2023-09-25 23:44:09 +0300

    js dosyası dışarıya alındı

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+const isim = "ahmet";
+var soyisim = "gülyaz";
+var tamisim = isim + " " + soyisim;
+var yas = 18;
+var yetişkinMi = yas == "24";
+var buyil = new Date().getFullYear();
+//isim = "mehmet";
+console.log(yetişkinMi);
+console.log("Merhaba Dünya!");
+console.log(++yas);
+console.log(isim, soyisim);
+console.log(tamisim, yas);
+console.log("Doğum Yılı", buyil + yas);
+
+// function scrollFn() {
+//   if (window.pageYOffset > 100) {
+//     document.getElementById("menu").classList.add("menu-scroll");
+//   } else {
+//     document.getElementById("menu").classList.remove("menu-scroll");
+//   }
+// }
